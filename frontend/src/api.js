a7b0a996e7b37ebc6436e1fcb33479e8460dde95
Vasudev <143492267+Vasu-Devs@users.noreply.github.com>
2025-09-04 01:49:29 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL : process.env.NEXT_PUBLIC_BACKEND_URL "http://localhost:8000"
+    baseURL : process.env.NEXT_PUBLIC_BACKEND_URL
 });
 export default api
